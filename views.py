9f92ac60a7211dee07a893156889c686bfc11a34
sanjaya9178 <78674322+sanjaya9178@users.noreply.github.com>
2021-10-25 15:25:27 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from .forms import UserForm
+from .models import UserItem
+from django.http import JsonResponse
 # Create your views here.
 
 def item_show(request):
@@ -11,8 +13,17 @@ def item_show(request):
     return render(request,'itemdisplay.html',{'form':fm})
 
 def ajax_call(request):
-    if request.method == 'POST':
-        item_no = request.POST.get('data')
-        print('iteam no',item_no)
 
-    return HttpResponse('success')
+    if request.method == 'POST':
+        item_no = request.POST.get('data1')
+        print('item no',item_no)
+        item_details=UserItem.objects.filter(item_number=item_no)
+        print(item_details)
+        dic = {}
+        for x in item_details:
+            dic['name'] = x.item_name
+            dic['number'] = x.item_number
+            dic['quentity'] = x.item_quantity
+        print(dic)
+
+    return JsonResponse(dic)
