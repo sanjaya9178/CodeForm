f146673a3ba67b2f5d05a54510de5ff9ddcb4edb
sanjaya9178 <78674322+sanjaya9178@users.noreply.github.com>
2021-10-25 10:07:29 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class UserItem(models.Model):
+    item_number=models.IntegerField()
+    item_name=models.CharField(max_length=30)
+    item_quantity=models.IntegerField()
