62b9534d37b53129a29a773b323c78852846dbbd
sanjaya9178 <78674322+sanjaya9178@users.noreply.github.com>
2021-10-26 13:12:32 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -6,3 +6,5 @@ class UserForm(forms.ModelForm):
     class Meta:
         model=UserItem
         fields=['item_number','item_name','item_quantity']
+
+        https://smallbusiness.chron.com/sending-things-printer-python-58655.html
